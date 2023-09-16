ba5a3744c1cb6fff530d9e78cd515f12cd08a884
Bond_vb28 <Bond_vb28@student.itstep.org>
2023-09-16 20:46:45 +0300

    del

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,62 +25,8 @@ namespace task2_16._09._23
         {
             InitializeComponent();
 
-            timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromSeconds(1);
-            timer.Tick += Timer_Tick;
-            timer.Start();
         }
         private DispatcherTimer timer;
       
-        private async void CreateProgressBars_Click(object sender, RoutedEventArgs e)
-        {
-            stackPanel_bar.Children.Clear();
-            int progressBarCount = GetProgressBarCountFromUser();
-            CreateAndAddProgressBars(progressBarCount);
-
-        }
-        private void Timer_Tick(object sender, EventArgs e)
-        {
-            foreach (ProgressBar progressBar in stackPanel_bar.Children)
-            {
-                if (progressBar != null)
-                {
-                    Random rand = new Random();
-                    int value = rand.Next(0, 100);
-                    progressBar.Dispatcher.Invoke(() =>
-                    {
-                        progressBar.Value = value;
-                    });
-                }
-            }
-        }
-        private int GetProgressBarCountFromUser()
-        {
-
-            int count = Convert.ToInt32(number_TB.Text.ToString());
-            return count;
-        }
-
-        private void CreateAndAddProgressBars(int count)
-        {
-            for (int i = 0; i < count; i++)
-            {
-                ProgressBar progressBar = new ProgressBar();
-                progressBar.Height = 30;
-                progressBar.Margin = new Thickness(10);
-                progressBar.Minimum = 0;
-                progressBar.Maximum = 100;
-                progressBar.Value = 0;
-                progressBar.Foreground = new SolidColorBrush(GetRandomColor());
-
-                stackPanel_bar.Children.Add(progressBar);
-            }
-        }
-
-        private Color GetRandomColor()
-        {
-            Random rand = new Random();
-            return Color.FromRgb((byte)rand.Next(256), (byte)rand.Next(256), (byte)rand.Next(256));
-        }
     }
 }
